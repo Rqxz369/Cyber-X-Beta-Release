2c3e191269d11f179ea282e4c2dce8980ce9a0eb
Rqxz369 <114289357+Rqxz369@users.noreply.github.com>
2022-09-24 21:42:37 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,31 +36,31 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['94702523487'] //ur owner number
+global.ownername = "𝕽𝖖𝖝𝖟369" //ur owner name
+global.ytname = "YT: RQXZ369" //ur yt chanel name
+global.socialm = "GitHub: rqxz369" //ur github or insta name
+global.location = "Sri Lanka, Kandy" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
-global.reactmoji = "😘" //ur menu react emoji
+global.owner = ['94702523487']
+global.ownertag = '94702523487' //ur tag number
+global.botnumber = '94702523487' //Add your Bot Number For Log
+global.botname = '𝕽𝖖𝖝𝖟𝕭𝖔𝖙369𝖎' //ur bot name
+global.ig = "https://www.instagram.com/" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.youtube.com/" //ur website to be displayed
+global.sc = 'https://github.com/' //script link
+global.linkz = 'https://github.com/'
+global.reactmoji = "⚙" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
-global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
-global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
-global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+global.packname = "𝕽𝖖𝖝𝖟𝕭𝖔𝖙369𝖎" //ur sticker watermark packname
+global.author = "𝕽𝖖𝖝𝖟369𝖎" //ur sticker watermark author
+global.footer = "® 𝕽𝖖𝖝𝖟369𝖎 ᴇᴅɪᴛɪᴏɴ"
+global.aliveimg ="https://img.uhdpaper.com/wallpaper/anonymous-hoodie-mask-digital-art-16@0@f-thumb.jpg?dl"
+global.allmenuimg ="https://free4kwallpapers.com/uploads/originals/2020/04/05/anonymous-wallpaper.jpg"
+global.welcomeimg= "https://img.freepik.com/premium-vector/anonymous-mascot-logo-esport-design-premium-vector_135814-194.jpg?w=2000"
+global.gbyeimg = "https://img.uhdpaper.com/wallpaper/anonymous-hoodie-mask-digital-art-16@0@f-thumb.jpg?dl"
+global.alivemsg = "ᕼEY TᕼEᖇE! I ᗩᗰ OᑎᒪIᑎE ᑎOᗯ :) \nᴵ ᵃᵐ ᵃ ᵘˢᵉʳ ᵇᵒᵗ ᶠᵒʳ ᵂʰᵃᵗˢᴬᵖᵖ \nʜᴀᴠᴇ ᴀ ɢʀᴇᴀᴛ ᴅᴀʏ !!!"
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94702523487'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
